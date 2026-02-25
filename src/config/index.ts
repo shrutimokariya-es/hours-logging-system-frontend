d417f69b6cffbaa2ffd6161185d562afdf4e4f94
Shruti-Avidly <shruti.mokariya@avidlyagnecy.com>
2026-02-25 18:18:20 +0530

    env changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,1 @@
-export const VITE_BACKEND_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+export const VITE_BACKEND_BASE_URL = process.env.VITE_APP_API_URL || 'http://localhost:5000/api';
